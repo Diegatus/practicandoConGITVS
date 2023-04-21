d9392cdc4e9d1396300384f08483dd1241504752
Diegatus <diegorugby1@hotmail.es>
2023-04-21 12:40:07 +0200

    hallo

--- Hola.java
+++ Hola.java
@@ -2,5 +2,6 @@ public class Hola {
     public static void main(String[] args) {
 	    System.out.println("Ey, qué tal");
 		System.out.println("Esto es otra linea mas");
+		System.out.println("Y otra más");
 	}
 }
